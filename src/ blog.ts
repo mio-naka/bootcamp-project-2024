5c2995833edd9f59f7bb0d29b9637859a8c97ba0
Mio <manakaga@calpoly.edu>
2024-10-15 14:43:07 -0700

    feat(blog): creating a list of Blog object

---
+++ src/ blog.ts
@@ -0,0 +1,28 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+};
+
+const blogs: Blog[] = [
+	{
+		title: "My first blog",
+        date: "10-15-2023",
+        description: "This is my first blog to write",
+        image: "IMG_6491.jpeg",
+        imageAlt: "image from home",
+        slug: "My first blog",
+	},
+	{
+		title: "My second blog",
+        date: "10-15-2023",
+        description: "This is my second blog to write :)",
+        image: "IMG_6491.jpeg",
+        imageAlt: "image from home",
+        slug: "My second blog",
+	},
+];
+
